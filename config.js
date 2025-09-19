7f08c1df22e974134506cad792686e9121352156
RafanSTR <153357481+RafanSTR@users.noreply.github.com>
2025-09-19 13:14:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const TELEGRAM_CONFIG = {
-    BOT_TOKEN: 'YOUR_BOT_TOKEN_HERE',  // Token dari @BotFather
-    CHAT_ID: 'YOUR_CHAT_ID_HERE'       // Chat ID Anda
+    BOT_TOKEN: '8287735024:AAH_GjQpdjHeBUUT-WEoCq6tMiFf_Kh108U',  // Token dari @BotFather
+    CHAT_ID: '6285234805'       // Chat ID Anda
 };
